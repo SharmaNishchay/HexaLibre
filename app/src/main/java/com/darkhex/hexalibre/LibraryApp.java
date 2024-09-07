1e2789ca79332ba44b3cb7a6ab05a80320f6aae9
SharmaNishchay <146124877+SharmaNishchay@users.noreply.github.com>
2024-09-07 16:49:09 +0530

    College load ui

--- app/src/main/java/com/darkhex/hexalibre/LibraryApp.java
+++ app/src/main/java/com/darkhex/hexalibre/LibraryApp.java
@@ -1,4 +1,36 @@
 package com.darkhex.hexalibre;
 
-public class LibraryApp {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Arrays;
+import java.util.List;
+
+public class LibraryApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Spinner setup for college selection
+        Spinner collegeSpinner = findViewById(R.id.spinner_colleges);
+
+        // List of colleges (can be dynamically generated)
+        List<String> collegeList = Arrays.asList(
+                "College of Engineering, Pune",
+                "Indian Institute of Technology, Bombay",
+                "Delhi Technological University",
+                "Birla Institute of Technology, Pilani",
+                "Vellore Institute of Technology"
+        );
+
+        // Create an ArrayAdapter to populate the Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, collegeList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the adapter to the Spinner
+        collegeSpinner.setAdapter(adapter);
+    }
 }
